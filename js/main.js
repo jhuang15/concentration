1dfabda5cd2facf60aacdfe05ab4e7e926c64937
jhuang15 <jchuang1020@gmail.com>
2022-04-04 14:16:05 -0400

    Adding in array of card colors and shuffle function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 /*----- constants -----*/
-
+const cardArr = ['red','orange', 'yellow', 'green', 'blue','purple','pink',
+'red','orange', 'yellow', 'green', 'blue','purple','pink']
 
 
 /*----- app's state (variables) -----*/
@@ -28,14 +29,23 @@ init();
 function init() {
   timer = '';
 
-
+  shuffleCards(cardArr); //Call shuffleCards to shuffle the colors in the array for each new board
   render();
 }
 
 function render() {
 
 }
-function handleCard() {
-  console.log("card clicked");
-  console.log(this);
+function handleCard(evt) {
+  evt.target.style.backgroundColor = 'red';
+}
+function shuffleCards(arr) {
+  let currentIdx = arr.length, randomIdx;
+  while (currentIdx !== 0){
+    randomIdx = Math.floor(Math.random() * currentIdx);
+    currentIdx--;
+
+    [arr[currentIdx], arr[currentIdx]] = [arr[randomIdx], arr[currentIdx]];
+  }
+  return arr;
 }
