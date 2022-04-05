d3a4e0fd3d5b2b58ebd3b46e32652addefb2f140
jhuang15 <jchuang1020@gmail.com>
2022-04-05 11:57:14 -0400

    Complete array constant

--- js/main.js
+++ js/main.js
@@ -1,17 +1,30 @@
 /*----- constants -----*/
-const cardArr = ['red','orange', 'yellow', 'green', 'blue','purple','pink',
-'red','orange', 'yellow', 'green', 'blue','purple','pink']
-
+const CARD_DECK = ['#F74305','#F74305', //red
+  '#F54D00','#F54D00', //orange red
+  '#F7BD05','#F7BD05', //orange
+  '#DEA60B','#DEA60B', //mustard yellow
+  '#F8CA81','#F8CA81', //beige
+  '#F5E801','#F5E801', //yellow
+  '#D7F884','#D7F884', //lime yellow
+  '#8CF08B','#8CF08B', //light green
+  '#0BDE51','#0BDE51', //green
+  '#6BFA00','#6BFA00', //neon green
+  '#00EDFA','#00EDFA', //light blue
+  '#8BB6F0','#8BB6F0', //dusty light blue
+  '#0567FA','#0567FA', //blue
+  '#8304D6','#8304D6', //purple 
+  '#DBB3F5','#DBB3F5', //light purple
+  '#FAAAE5','#FAAAE5', //light pink
+  '#F88681','#F88681', //blush pink
+  '#F8886B','#F8886B', //salmon
+  '#EC05FA','#EC05FA'] //hot pink
 
 /*----- app's state (variables) -----*/
 let board; //An array of the board cards
 let timer; //
 let score;
-
-
-
-
-
+let firstCard, secondCard;
+let flipped;
 
 /*----- cached element references -----*/
 const cards = document.querySelectorAll('.card');
@@ -21,23 +34,24 @@ cards.forEach (function (card) {
   card.addEventListener('click', handleCard);
 });
 
-
-
 /*----- functions -----*/
 init();
 
 function init() {
   timer = '';
 
-  shuffleCards(cardArr); //Call shuffleCards to shuffle the colors in the array for each new board
+  shuffleCards(CARD_DECK); //Call shuffleCards to shuffle the colors in the array for each new board
   render();
 }
 
 function render() {
 
 }
+
 function handleCard(evt) {
-  evt.target.style.backgroundColor = 'red';
+  
+
+  evt.target.style.backgroundColor = CARD_DECK[1];
 }
 
 //Function takes in an array parameter, while the array length is NOT empty 
