c8d589c4c53953707ef4d525bf9e1305fa66eeb9
jhuang15 <jchuang1020@gmail.com>
2022-04-05 15:36:01 -0400

    Adjust 2D array, included countdown function

--- js/main.js
+++ js/main.js
@@ -1,51 +1,68 @@
 /*----- constants -----*/
-const CARD_DECK = ['#F74305','#F74305', //red
-  '#F54D00','#F54D00', //orange red
-  '#F7BD05','#F7BD05', //orange
-  '#DEA60B','#DEA60B', //mustard yellow
-  '#F8CA81','#F8CA81', //beige
-  '#F5E801','#F5E801', //yellow
-  '#D7F884','#D7F884', //lime yellow
-  '#8CF08B','#8CF08B', //light green
-  '#0BDE51','#0BDE51', //green
-  '#6BFA00','#6BFA00', //neon green
-  '#00EDFA','#00EDFA', //light blue
-  '#8BB6F0','#8BB6F0', //dusty light blue
-  '#0567FA','#0567FA', //blue
-  '#8304D6','#8304D6', //purple 
-  '#DBB3F5','#DBB3F5', //light purple
-  '#FAAAE5','#FAAAE5', //light pink
-  '#F88681','#F88681', //blush pink
-  '#F8886B','#F8886B', //salmon
-  '#EC05FA','#EC05FA'] //hot pink
+const CARD_DECK = [
+  ['#F74305','#F74305',//red
+  '#F54D00','#F54D00',//orange red
+  '#F7BD05','#F7BD05'],//orange
+  ['#DEA60B','#DEA60B',//mustard yellow
+  '#F8CA81','#F8CA81',//beige
+  '#F5E801','#F5E801'],//yellow
+  ['#D7F884','#D7F884',//lime yellow
+  '#8CF08B','#8CF08B',//light green
+  '#0BDE51','#0BDE51'],//green
+  ['#6BFA00','#6BFA00',//neon green
+  '#00EDFA','#00EDFA',//light blue
+  '#8BB6F0','#8BB6F0'],//dusty light blue
+  ['#0567FA','#0567FA',//blue
+  '#8304D6','#8304D6',//purple 
+  '#DBB3F5','#DBB3F5'],//light purple
+  ['#FAAAE5','#FAAAE5',//light pink
+  '#F88681','#F88681',//blush pink
+  '#F8886B','#F8886B'],//salmon
+  ]; 
 
 /*----- app's state (variables) -----*/
 let board; //An array of the board cards
-let timer; //
-let score;
-let firstCard, secondCard;
-let flipped;
-
+let firstCard, secondCard; //An array index is assigned to that varible 
+let flipped; // 'T' -> the card has flipped, 'F' -> the card has not been clicked
+let gameStatus; // 'W' -> Win; 'L' -> Loss; null -> game in progress
+let timeLeft;
 /*----- cached element references -----*/
-const cards = document.querySelectorAll('.card');
+const cardEl = document.querySelectorAll('.card');
+const playBtn = document.getElementById('play-btn');
+const timeEl = document.getElementById('time');
+const scoreEl = document.getElementById('score');
 
 /*----- event listeners -----*/
-cards.forEach (function (card) {
+cardEl.forEach (function (card) {
   card.addEventListener('click', handleCard);
 });
+
+playBtn.addEventListener('click', init);
 
 /*----- functions -----*/
 init();
 
 function init() {
-  timer = '';
-
-  shuffleCards(CARD_DECK); //Call shuffleCards to shuffle the colors in the array for each new board
+  board = [
+  [null, null, null, null, null, null], 
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null], 
+  [null, null, null, null, null, null], 
+  [null, null, null, null, null, null]
+  ]; 
+  firstCard = null; 
+  secondCard = null;
+  flipped = null;
+  gameStatus = null;
+  timeLeft = 30; //set the countdown timer
+  
   render();
 }
 
 function render() {
-
+  let timeoutId = setInterval(countdown, 1000);
+  shuffleCards(CARD_DECK); //Call shuffleCards to shuffle the colors in the array for each new board
 }
 
 function handleCard(evt) {
@@ -68,4 +85,15 @@ function shuffleCards(arr) {
     arr[randIdx] = tmp;
   }
   return arr;
+}
+
+function countdown() {
+  if (timeLeft === -1) {
+    clearTimeout(timeoutId);
+    flipCards(); //Invoke function to flip cards over and start timing game
+  } else {
+    timeEl.innerHTML = timeLeft + ' seconds remaining';
+    timeLeft--;
+  }
+  
 }
