6b7181e12a0a822ee00bb34861ac66c3f9a3c89d
jhuang15 <jchuang1020@gmail.com>
2022-04-07 19:27:51 -0400

    Remove unnecessary comments, alter countdown to stopwatch function

--- js/main.js
+++ js/main.js
@@ -1,47 +1,50 @@
 /*----- constants -----*/
-  const CARD_DECK = [
-    {hex:'#F74305', matched: false},
-    {hex:'#F54D00', matched: false},
-    {hex:'#F7BD05', matched: false},
-    {hex:'#DEA60B', matched: false},
-    {hex:'#F8CA81', matched: false},
-    {hex:'#F5E801', matched: false},
-    {hex:'#D7F884', matched: false},
-    {hex:'#8CF08B', matched: false},
-    {hex:'#0BDE51', matched: false},
-    {hex:'#6BFA00', matched: false},
-    {hex:'#00EDFA', matched: false},
-    {hex:'#8BB6F0', matched: false},
-    {hex:'#0567FA', matched: false},
-    {hex:'#8304D6', matched: false},
-    {hex:'#DBB3F5', matched: false},
-    {hex:'#FAAAE5', matched: false},
-    {hex:'#F88681', matched: false},
-    {hex:'#F8886B', matched: false},
-  ];
-
-  const NULL_CARD = '#D5D5D5';
+const CARD_DECK = [
+  {hex:'#F74305', matched: false},
+  {hex:'#F54D00', matched: false},
+  {hex:'#F7BD05', matched: false},
+  {hex:'#DEA60B', matched: false},
+  {hex:'#F8CA81', matched: false},
+  {hex:'#F5E801', matched: false},
+  {hex:'#D7F884', matched: false},
+  {hex:'#8CF08B', matched: false},
+  {hex:'#0BDE51', matched: false},
+  {hex:'#6BFA00', matched: false},
+  {hex:'#00EDFA', matched: false},
+  {hex:'#8BB6F0', matched: false},
+  {hex:'#0567FA', matched: false},
+  {hex:'#8304D6', matched: false},
+  {hex:'#DBB3F5', matched: false},
+  {hex:'#FAAAE5', matched: false},
+  {hex:'#F88681', matched: false},
+  {hex:'#F8886B', matched: false},
+];
+const NULL_CARD = '#D5D5D5';
 
 /*----- app's state (variables) -----*/
 let cards; // Array of 36 shuffled card objects
 let firstCard; //first card clicked (card object) or null
 let secondCard;
 let ignoreClicks; 
-let timeLeft;
-let thisTimeout; //for 1 second timer in handleChoice function
+let score;
+let mins;
+let seconds;
+let interval;
+
 /*----- cached element references -----*/
 const cardEl = [...document.querySelectorAll('.card')];
 const playBtn = document.getElementById('play-btn');
 const timeEl = document.getElementById('time');
-const instrEl = document.getElementById('instructions');
 const scoreEl = document.getElementById('score');
+const secondsEl = document.getElementById('seconds');
+const minsEl = document.getElementById('mins');
 
 /*----- event listeners -----*/
-
 playBtn.addEventListener('click', init);
 cardEl.forEach (function (card) {
-  card.addEventListener('click', handleChoice)
+  card.addEventListener('click', handleChoice) //ALSO BEGIN STOPWATCH WHEN CARD CLICKED
 });
+
 /*----- functions -----*/
 init();
 
@@ -50,14 +53,15 @@ function init() {
   secondCard = null;
   ignoreClicks = false;
   cards = getShuffledCards();
-
-  timeLeft = 30;
-  //instrEl.setAttribute('hidden', true); //hide instruction when game is in play
+  score = 0; 
+  mins = 00;
+  seconds = 00;
   render();
-}
+  }
 
 function render() {
-  let timeoutId = setInterval(countdown, 1000);
+  clearInterval(interval);
+  interval = setInterval(stopwatch, 1000);
 
   cards.forEach(function(card, idx){
     const colorDiv = document.getElementById(idx);
@@ -66,35 +70,41 @@ function render() {
     colorDiv.hex = hex;
     colorDiv.setAttribute('style', `background-color: ${colorDiv.hex}`); //sets the board to default gray 
   });
-
-  
+  //instrEl.style.visibility = true; //hide instruction when game is in play
 }
 
-function renderHide(){
-  
-  console.log(instrEl);
+function stopwatch() {
+  seconds++;
+  if (seconds <= 9){
+    secondsEl.innerHTML = '0' + seconds;
+  }
+  if (seconds > 9){
+    secondsEl.innerHTML = seconds;
+  }
+  if (seconds > 60) {
+    mins++;
+    minsEl.innerHTML = '0' +mins;
+    seconds = 0;
+    secondsEl.innerHTML = '0' + 0;
+  }
+  if (mins >9){
+    minsEl.innerHTML = mins;
+  }
+  render();
 }
 
 function handleChoice(evt) {
   const cardIdx = parseInt(evt.target.id); 
-
   cardEl[cardIdx].style.backgroundColor = cards[cardIdx].hex; // !!!!DONT TOUCH !!!!
-  //console.log(cardEl[cardIdx]);
   
   if (isNaN(cardIdx) || ignoreClicks) return;
   let secondCard = cards[cardIdx]; //NEW added in .hex
   if (firstCard){
-   
     if (firstCard.hex === secondCard.hex) { 
       firstCard.matched = secondCard.matched = true;
-      console.log('firstCard.color -->'+firstCard.hex);
-      console.log('secondCard.color -->'+secondCard.hex);
-      console.log('firstCard.matched -->'+firstCard.matched);
-      console.log('secondCard.matched -->'+secondCard.matched);
-      
-      //!!!call score function here to increase score!!!
-
+      renderScore();
     } else {
+      
       //set the card to true so it'll show for one second
       //thisTimeout = setTimeout(function(){
        // secondTimer();
@@ -102,23 +112,17 @@ function handleChoice(evt) {
        // console.log(cards[cardIdx])
        // clearTimeout(thisTimeout);
       //}, 1000);
-
     }
     firstCard = null;
-    console.log('firstCard=null -->'+firstCard);
   } else {
     firstCard = secondCard;
-    console.log('firstCard=Secondcard -->'+firstCard);
   }
-
-  
+  //timer for both unmatched cards to show temp.
   function secondTimer() {
     clearTimeout(thisTimeout);
     firstCard.matched = true;
     secondCard.matched = true;
   }
-  
-  
   render();
 }
 
@@ -136,13 +140,11 @@ function getShuffledCards() {
   return cards;
 }
 
-//MY SHUFFLE FUNCTION 
+//OLD SHUFFLE FUNCTION 
 function shuffleCards(arr) {
   let m = arr.length;
-
   while(m !== 0) {
     let randIdx = Math.floor(Math.random() * m--);
-
     //use temporary variable to swap index 
     let tmp = arr[m];
     arr[m] = arr[randIdx];
@@ -150,22 +152,13 @@ function shuffleCards(arr) {
   }
   return arr;
 }
-function countdown() {
-   //move this later, timer wont run
-  if (timeLeft === -1) {
-    clearTimeout(timeoutId);
-     //Invoke function to flip cards over and start timing game
-  } else {
-    timeEl.innerHTML = timeLeft + ' seconds remaining';
-    timeLeft--;
-  }
-}
-
-function flipCards () {
-
-}
 
 function renderScore(){
-  
+  if (score === 17) {
+    scoreEl.innerText = `Completed board in ${getTime}`;
+  } else {
+    scoreEl.innerHTML = `Score: ${++score}`;
+  }
+  render();//might need to all render here???? 
 }
 
